8880bf54eefac9078d46d930c6b4982363096150
влад тюрин <vladislav-tyurin@bk.ru>
2022-11-15 20:25:16 +0300

    Добавьте файлы проекта.

---
+++ Algorithms/FastExponentiation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public static class FastExponentiation
+    {
+        public static long PowerRecursion(long x, uint n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            if ((n & 1) == 0)
+            {
+                var p = Power(x, n >> 1);
+                return p * p;
+            }
+            else
+            {
+                return x * Power(x, n - 1);
+            }
+        }
+
+        public static long Power(long x, uint n)
+        {
+            var result = 1L;
+            while(n > 0)
+            {
+                if( (n & 1) == 0)
+                {
+                    x *= x;
+                    n >>= 1;
+                }
+                else
+                {
+                    result *= x;
+                    n--;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ AlgorithmsConsoleTest/Program.cs
@@ -0,0 +1,8 @@
+﻿
+using Algorithms;
+
+var result = LevenshteinDistance.GetLevenshteinDistance("Пермь", "Сперма");
+
+Console.WriteLine(result);
+
+Console.ReadKey();
