290d566b496c2eda1bbf03ca5c8c72c8608dddc1
влад тюрин <vladislav-tyurin@bk.ru>
2022-11-21 08:42:28 +0300

    Обычный поиск

---
+++ Algorithms/DumbSearch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public static class DumbSearch
+    {
+        public static int? Search(int[] array, int value)
+        {
+            foreach (var item in array)
+            {
+                if(item == value)
+                    return item;
+            }
+            return null;
+        }
+    }
+}
