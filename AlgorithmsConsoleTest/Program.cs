cf0178d96ed09d139990b96b09138d7c9024dcc4
влад тюрин <vladislav-tyurin@bk.ru>
2022-11-21 08:43:08 +0300

    Тест

--- AlgorithmsConsoleTest/Program.cs
+++ AlgorithmsConsoleTest/Program.cs
@@ -1,15 +1,4 @@
 ﻿
 using Algorithms;
 
-
-Console.Write("Введите элементы массива: ");
-var parts = Console.ReadLine().Split(new[] { " ", ",", ";" }, StringSplitOptions.RemoveEmptyEntries);
-var array = new int[parts.Length];
-for (int i = 0; i < parts.Length; i++)
-{
-    array[i] = Convert.ToInt32(parts[i]);
-}
-
-Console.WriteLine("Отсортированный массив: {0}", string.Join(", ", ShakerSort.Sort(array)));
-
 Console.ReadLine();
