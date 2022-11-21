0838f9bfc90e8e6bf0db6e0ff6924149df369fbf
влад тюрин <vladislav-tyurin@bk.ru>
2022-11-21 08:42:37 +0300

    Рекурсивный факториал

---
+++ Algorithms/Factorial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public static class Factorial
+    {
+        public static ulong Calculate(ulong x)
+        {
+            if (x == 1uL)
+                return 1uL;
+            else
+                return Calculate(x - 1uL) * x;
+        }
+    }
+}
